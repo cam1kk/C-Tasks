39ab200a67803437071f126281e35dbc70116097
Бабаев Джамал Чингиз <Babae_me80@itstep.edu.az>
2024-03-12 18:48:41 +0400

    updd

--- C# Tasks/Program.cs
+++ C# Tasks/Program.cs
@@ -50,17 +50,42 @@
 //}
 #endregion
 
-#region Task3 
-Console.WriteLine("Vvedite 4 chisla:");
-string input = Console.ReadLine();
+#region Task 3 
+//Console.WriteLine("Vvedite 4 chisla:");
+//string input = Console.ReadLine();
 
-if (input.Length == 4 && int.TryParse(input, out _))
-{
-    int number = int.Parse(input);
-    Console.WriteLine($"Number: {number}");
-}
-else
+//if (input.Length == 4 && int.TryParse(input, out _))
+//{
+//    int number = int.Parse(input);
+//    Console.WriteLine($"Number: {number}");
+//}
+//else
+//{
+//    Console.WriteLine("Error");
+//}
+#endregion
+
+#region Task 4
+string number = Console.ReadLine();
+if (number.Length != 6)
 {
     Console.WriteLine("Error");
 }
+
+string pos1 = Console.ReadLine();
+string pos2 = Console.ReadLine();
+if (pos1.Length != 1 && pos2.Length != 1)
+{
+    Console.WriteLine("Error");
+}
+int poss1 = int.Parse(pos1);
+int poss2 = int.Parse(pos2);
+
+char[] digits = number.ToCharArray();
+char temp = digits[poss1 - 1];
+digits[poss1 - 1] = digits[poss2 - 1];
+digits[poss2 - 1] = temp;
+
+string result = new string(digits);
+Console.WriteLine("Результат обмена цифр: " + result);
 #endregion
